6f82745fec531d2945565fdeefc28c47ceb09b14
L1ang <462207840@qq.com>
2024-01-09 23:24:46 +0800

    refine application

--- Application.py
+++ Application.py
@@ -1,35 +1,40 @@
 import tkinter as tk
 from tkinter import filedialog
+import Plot
+import ExcelLoader
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# 创建主界面
-root = tk.Tk()
+class Application:
+    def __init__(self):
+        self.app = tk.Tk()
+        self.create()
 
-# 设置窗口标题
-root.title("主界面")
+    def create(self):
+        self.app.title("Trade")
+        # 创建按钮
+        self.button = tk.Button(self.app, text="选择文件", command=self.open_file_dialog)
+        self.button.pack()
+        # 创建矩形区域并在矩形区域上绘制曲线
+        self.figure = plt.figure(figsize=(5, 4), dpi=100)
+        self.axes = self.figure.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.app)
+        self.canvas.get_tk_widget().pack()
 
-# 创建按钮点击事件
-def open_file_dialog():
-    # 打开文件选择对话框
-    file_path = filedialog.askopenfilename()
-    print("选择的文件:", file_path)
+    # 创建按钮点击事件
+    def open_file_dialog(self):
+        # 打开文件选择对话框
+        self.file_path = filedialog.askopenfilename()
+        print("选择的文件:", self.file_path)
+        self.loader = ExcelLoader.myExcelLoader(self.file_path)
+        self.draw_canvas()
 
-# 创建按钮
-button = tk.Button(root, text="选择文件", command=open_file_dialog)
-button.pack()
+    def run(self):
+        self.app.mainloop()
+        self.draw_canvas()
 
-# 创建矩形区域
-figure = plt.figure(figsize=(5, 4), dpi=100)
-axes = figure.add_subplot(111)
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
-axes.plot(x, y)
-
-# 在矩形区域上绘制曲线
-canvas = FigureCanvasTkAgg(figure, master=root)
-canvas.draw()
-canvas.get_tk_widget().pack()
-
-# 运行主界面
-root.mainloop()
+    def draw_canvas(self):
+        data = self.loader.get_data()
+        self.axes.clear()
+        self.axes.plot(data.iloc[:, 0], data.iloc[:, 1])
+        self.canvas.draw()
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import Plot
 import ExcelLoader
+import Application
 
 #TODO 从gui输入excel文件
-loader = ExcelLoader.myExcelLoader('AWP.xlsx')
-loader.get_data()
+#loader = ExcelLoader.myExcelLoader('AWP.xlsx')
+#loader.get_data()
 
-plot = Plot.myPlot(loader.get_data())
-plot.draw()
+#plot = Plot.myPlot(loader.get_data())
+#plot.draw()
 
-
+app = Application.Application()
+app.run()
 
 #TODO
 #硬编码回测策略
